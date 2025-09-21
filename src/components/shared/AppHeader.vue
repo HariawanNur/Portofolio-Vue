80d5692e22deaa04db36e6a4f3a0988ad48a451c
hariawannurfajri <v.hariawan.fajri@adira.co.id>
2025-09-21 10:24:35 +0700

    enhance project

--- src/components/shared/AppHeader.vue
+++ src/components/shared/AppHeader.vue
@@ -70,6 +70,23 @@ export default {
 		<div
 			class="z-10 max-w-screen-lg xl:max-w-screen-xl block sm:flex sm:justify-between sm:items-center my-6"
 		>
+
+						<div>
+					<router-link to="/"
+						><img
+							v-if="theme === 'light'"
+							src="@/assets/images/logo-dark.svg"
+							class="w-36"
+							alt="Dark Logo"
+						/>
+						<img
+							v-else
+							src="@/assets/images/logo-light.svg"
+							class="w-36"
+							alt="Light Logo"
+						/>
+					</router-link>
+				</div>
 			<!-- Header menu links and small screen hamburger menu -->
 			<div class="flex justify-between items-center px-4 sm:px-0">
 				<!-- Theme switcher small screen -->
--- src/data/projects.js
+++ src/data/projects.js
@@ -4,39 +4,39 @@
 const projects = [
 	{
 		id: 1,
-		title: 'Google Health Platform',
+		title: 'AccountCheck',
 		category: 'Web Application',
 		img: require('@/assets/images/web-project-2.jpg'),
 	},
 	{
 		id: 2,
-		title: 'Phoenix Digital Agency',
+		title: 'XSS Shield',
 		category: 'Mobile Application',
 		img: require('@/assets/images/mobile-project-2.jpg'),
 	},
 	{
 		id: 3,
-		title: 'Project Management UI',
-		category: 'UI/UX Design',
-		img: require('@/assets/images/ui-project-1.jpg'),
-	},
-	{
-		id: 4,
-		title: 'Cloud Storage Platform',
-		category: 'UI/UX Design',
-		img: require('@/assets/images/ui-project-2.jpg'),
-	},
-	{
-		id: 5,
-		title: 'React Social App',
+		title: 'FortiPass: Secure Mobile Forgot Password System',
 		category: 'Mobile Application',
 		img: require('@/assets/images/mobile-project-1.jpg'),
 	},
 	{
-		id: 6,
-		title: 'Apple Design System',
+		id: 4,
+		title: 'PassRenew: Secure Admin-Initiated Password Reset System',
 		category: 'Web Application',
 		img: require('@/assets/images/web-project-1.jpg'),
+	},
+	{
+		id: 5,
+		title: 'AuthShield: Secure Login Validation System',
+		category: 'Web Application',
+		img: require('@/assets/images/ui-project-2.jpg'),
+	},
+	{
+		id: 6,
+		title: 'Cloudinary-Powered Media Uploader',
+		category: 'Web Application',
+		img: require('@/assets/images/ui-project-2.jpg'),
 	},
 ];
 
